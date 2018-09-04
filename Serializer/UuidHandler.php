81e5abcea60751a3e812018fc9a428560984304c
Marek Štípek <mario.dweller@seznam.cz>
2018-09-04 18:16:13 +0200

    Support of serialization of UuidInterface instead of just Uuid

--- Serializer/UuidHandler.php
+++ Serializer/UuidHandler.php
@@ -1,13 +1,18 @@
 <?php
 namespace Vanio\ApiBundle\Serializer;
 
+use JMS\Serializer\EventDispatcher\Event;
+use JMS\Serializer\EventDispatcher\Events;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\PreDeserializeEvent;
+use JMS\Serializer\EventDispatcher\PreSerializeEvent;
 use JMS\Serializer\GraphNavigator;
 use JMS\Serializer\Handler\SubscribingHandlerInterface;
 use JMS\Serializer\VisitorInterface;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
-class UuidHandler implements SubscribingHandlerInterface
+class UuidHandler implements SubscribingHandlerInterface, EventSubscriberInterface
 {
     public function serialize(VisitorInterface $visitor, Uuid $uuid): string
     {
@@ -17,6 +22,16 @@ class UuidHandler implements SubscribingHandlerInterface
     public function deserialize(VisitorInterface $visitor, string $value): UuidInterface
     {
         return Uuid::fromString($value);
+    }
+
+    public function onPreSerialize(PreSerializeEvent $event): void
+    {
+        $this->assignUuidTypeName($event);
+    }
+
+    public function onPreDeserialize(PreDeserializeEvent $event): void
+    {
+        $this->assignUuidTypeName($event);
     }
 
     /**
@@ -35,7 +50,7 @@ class UuidHandler implements SubscribingHandlerInterface
             foreach ($methods as $direction => $method) {
                 $subscribingMethods[] = [
                     'direction' => $direction,
-                    'type' => Uuid::class,
+                    'type' => UuidInterface::class,
                     'format' => $format,
                     'method' => $method,
                 ];
@@ -43,5 +58,26 @@ class UuidHandler implements SubscribingHandlerInterface
         }
 
         return $subscribingMethods;
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ['event' => Events::PRE_SERIALIZE, 'method' => 'onPreSerialize'],
+            ['event' => Events::PRE_DESERIALIZE, 'method' => 'onPreDeserialize'],
+        ];
+    }
+
+    /**
+     * @param PreSerializeEvent|PreDeserializeEvent $event
+     */
+    private function assignUuidTypeName(Event $event): void
+    {
+        if ($event->getObject() instanceof UuidInterface) {
+            $event->setType(UuidInterface::class, [get_class($event->getObject())]);
+        }
     }
 }
