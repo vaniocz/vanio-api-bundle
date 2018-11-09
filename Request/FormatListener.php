6f30136e1ac9666e6041edf8d75e8a2856845a9f
Marek Štípek <mario.dweller@seznam.cz>
2018-11-09 02:41:34 +0100

    Fixing FormatListener when router has strict requirements

--- Request/FormatListener.php
+++ Request/FormatListener.php
@@ -5,23 +5,35 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\ConfigurableRequirementsInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\Router;
 
 class FormatListener implements EventSubscriberInterface
 {
     /** @var UrlGeneratorInterface */
     private $urlGenerator;
 
+    /** @var UrlGeneratorInterface|null */
+    private $configurableUrlGenerator;
+
     /** @var string[] */
     private $defaultAcceptableFormats;
 
     /**
+     * FormatListener constructor.
      * @param UrlGeneratorInterface $urlGenerator
+     * @param UrlGeneratorInterface|null $defaultUrlGenerator
      * @param string[] $defaultAcceptableFormats
      */
-    public function __construct(UrlGeneratorInterface $urlGenerator, array $defaultAcceptableFormats)
-    {
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        ?UrlGeneratorInterface $defaultUrlGenerator = null,
+        array $defaultAcceptableFormats = []
+    ) {
         $this->urlGenerator = $urlGenerator;
+        $this->configurableUrlGenerator = $this->resolveConfigurableRequirementsUrlGenerator($urlGenerator)
+            ?: $this->resolveConfigurableRequirementsUrlGenerator($defaultUrlGenerator);
         $this->defaultAcceptableFormats = $defaultAcceptableFormats;
     }
 
@@ -48,6 +60,17 @@ class FormatListener implements EventSubscriberInterface
         return [KernelEvents::REQUEST => 'onRequest'];
     }
 
+    private function resolveConfigurableRequirementsUrlGenerator(UrlGeneratorInterface $urlGenerator): ?ConfigurableRequirementsInterface
+    {
+        if ($urlGenerator instanceof ConfigurableRequirementsInterface) {
+            return $urlGenerator;
+        } elseif ($urlGenerator instanceof Router) {
+            return $this->resolveConfigurableRequirementsUrlGenerator($urlGenerator->getGenerator());
+        }
+
+        return null;
+    }
+
     private function isFormatPresentInRoutePattern(Request $request): bool
     {
         if (!$request->attributes->has('_route')) {
@@ -56,7 +79,18 @@ class FormatListener implements EventSubscriberInterface
 
         $routeParameters = $request->attributes->get('_route_params') ?? [];
         $parameters = ['_format' => '__FORMAT__'] + $routeParameters;
+
+        if ($this->configurableUrlGenerator) {
+            $isStrictRequirements = $this->configurableUrlGenerator->isStrictRequirements();
+            $this->configurableUrlGenerator->setStrictRequirements(null);
+        }
+
         $url = $this->urlGenerator->generate($request->attributes->get('_route'), $parameters);
+
+        if (isset($isStrictRequirements)) {
+            $this->configurableUrlGenerator->setStrictRequirements($isStrictRequirements);
+        }
+
         parse_str(parse_url($url, PHP_URL_QUERY), $additionalAttributes);
 
         return !isset($additionalAttributes['_format']);
