7b5d2c6f74c0c71d825561347475b3e35065867f
Marek Štípek <mario.dweller@seznam.cz>
2019-10-25 16:28:00 +0200

    Fixing FilterParamConverter after updating VanioDomainBundle

--- Request/FilterParamConverter.php
+++ Request/FilterParamConverter.php
@@ -4,32 +4,37 @@ namespace Vanio\ApiBundle\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Translation\TranslatorInterface;
 use Vanio\ApiBundle\Specification\Filter;
 use Vanio\DomainBundle\Pagination\OrderByParamConverter;
 
 class FilterParamConverter implements ParamConverterInterface
 {
+    /** @var TranslatorInterface */
+    private $translator;
+
     /** @var mixed[] */
     private $options;
 
     /**
      * @param mixed[] $options
      */
-    public function __construct(array $options = [])
+    public function __construct(TranslatorInterface $translator, array $options = [])
     {
+        $this->translator = $translator;
         $this->options = $options + ['dql_alias' => null];
     }
 
     public function apply(Request $request, ParamConverter $configuration): bool
     {
         $options = $configuration->getOptions() + $this->options;
-        (new OrderByParamConverter($options))->apply($request, $configuration);
+        (new OrderByParamConverter($this->translator, $options))->apply($request, $configuration);
         $orderBy = $request->attributes->get($configuration->getName());
         (new LimitParamConverter($options))->apply($request, $configuration);
         $limit = $request->attributes->get($configuration->getName());
-        (new PropertiesParamConverter($options))->apply($request, $configuration);
+        (new PropertiesParamConverter)->apply($request, $configuration);
         $properties = $request->attributes->get($configuration->getName());
-        (new QueryParamConverter($options))->apply($request, $configuration);
+        (new QueryParamConverter)->apply($request, $configuration);
         $query = $request->attributes->get($configuration->getName());
         $filter = new Filter($orderBy, $limit, $properties, $query, $options['dql_alias']);
         $request->attributes->set($configuration->getName(), $filter);
